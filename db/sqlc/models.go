9927d6237948335558be6affb40a0d8632c0a0f7
renato@likwi.com.br <renatofagalde>
2024-05-30 17:52:25 -0700

    user db, test and controller

--- db/sqlc/models.go
+++ db/sqlc/models.go
@@ -35,3 +35,12 @@ type Transfer struct {
 	// must be positive
 	Amount int64 `json:"amount"`
 }
+
+type User struct {
+	Username          string    `json:"username"`
+	HashedPassword    string    `json:"hashed_password"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
