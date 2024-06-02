9488d79b076890dda49e89bd562e7bd50c41d558
renato@likwi.com.br <renatofagalde>
2024-06-02 07:09:27 -0700

    TestExpíredJWTToken

--- token/jwtMaker_test.go
+++ token/jwtMaker_test.go
@@ -31,3 +31,18 @@ func TestJWTMaker(t *testing.T) {
 	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
 
 }
+
+func TestExpíredJWTToken(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Empty(t, payload)
+
+}
