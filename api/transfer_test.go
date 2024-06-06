f003b2c8dbb2efb3d3e286d0501b1ea8deda6444
renato@likwi.com.br <renatofagalde>
2024-06-05 17:10:03 -0700

    test 🤡 validation

--- api/transfer_test.go
+++ api/transfer_test.go
@@ -112,12 +112,12 @@ func TestTransferAPI(t *testing.T) {
 		//		"currency":        util.USD,
 		//	},
 		//	setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
-		//		addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user1.Username, user1.Role, time.Minute)
+		//		addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user1.Username, time.Minute)
 		//	},
 		//	buildStubs: func(store *mockdb.MockStore) {
 		//		store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(db.Account{}, db.ErrRecordNotFound)
 		//		store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(0)
-		//		store.EXPECT().TransferTx(gomock.Any(), gomock.Any()).Times(0)
+		//		store.EXPECT().Transfer(gomock.Any(), gomock.Any()).Times(0)
 		//	},
 		//	checkResponse: func(recorder *httptest.ResponseRecorder) {
 		//		require.Equal(t, http.StatusNotFound, recorder.Code)
