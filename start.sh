7b25b3e4415cfa136833a10309ab8278a5736e22
renato@likwi.com.br <renatofagalde>
2024-06-08 13:54:06 -0700

    💐 change secret-id name

--- start.sh
+++ start.sh
@@ -2,11 +2,20 @@
 
 set -e
 
-echo "run db migration 05"
+echo "run db migration 06"
+echo "ls /app ----"
 ls -lh /app
+echo " ----"
 source /app/app.env
-cat /app/app.env
+
+echo "tail -n 1000 /app/app.env ----"
+tail -n 1000 /app/app.env
 echo $DB_SOURCE
+
+echo "ls /app/migration ----"
+ls -lh /app/migration
+echo " ----"
+
 /app/migrate -path /app/migration -database ${DB_SOURCE} -verbose up
 
 echo "start the app"
